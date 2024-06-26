c6c604a59f9746421208e25ce6a2a24c1cfbf7b8
Aryan Chaudhari <ar321922@dal.ca>
2024-06-26 13:03:50 -0300

    Adding dummy home page

--- frontend/dal-social/src/App.js
+++ frontend/dal-social/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignUp from './components/signup';
 import Login from './components/login';
+import Homepage from "./components/homepage";
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path = "/"></Route>
           <Route path = "/signup" element={<SignUp/>}/>
           <Route path = "/login" element={<Login/>}/>
+            <Route path="/homepage" element={<Homepage/>} />
         </Routes>
       </Router>
     </div>
