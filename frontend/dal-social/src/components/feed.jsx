06da87ff6e4667f4afab48083c063a9afd8347e3
Kaedan <kd577631@dal.ca>
2024-06-27 20:25:23 -0300

    feat: added frontend posting interface

--- frontend/dal-social/src/components/feed.jsx
+++ frontend/dal-social/src/components/feed.jsx
@@ -1,13 +1,51 @@
 import React, { useRef, useState, useEffect } from 'react';
 import '../css/feed.css';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Link, useNavigate} from 'react-router-dom';
 
 const Feed = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const[content, setContent] = useState('');
+    const[postDate, setPostDate] = useState('');
+    const[title, setTitle] = useState('');
+    const userId = '';
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = {
+            content,
+            postDate,
+            title,
+            userId
+        }
+
+        try{
+            const email = localStorage.getItem('loggedInUser');
+            const user = axios.get("http://localhost:8080/users/getbyemail/" + email);
+            userId = user.data.id;
+
+            //add storing into the database
+            //const response = await axios.post("http://localhost:8080:users/save")
+        }
+        catch(error) {
+            console.error("There was an erroring fetching the user id", error);
+            alert("There was an erroring fetching the user id");
+        }
+
+
+
+    }
+
     return (
         <div className='basicWrapper'>
             <h1>DALSOCIAL</h1>
             <h2>DALHOUSIE SOCIAL NETWORK</h2>
-            <h3>This is the feed page (in development)</h3>
 
             <nav className='nav'>
                 <Link to="/signup" className='nav-item'>
@@ -17,8 +55,18 @@ const Feed = () => {
                 <Link to="/login" className='nav-item'>
                     <a href="#">Login</a>
                 </Link>
+                
             </nav>
             
+            <p>This is the feed page (in development)</p>
+
+            <form onSubmit={handleSubmit}>
+                <label>Create your post!</label>
+                <input type='text' placeholder='Title' id='input-title' onChange={(e) => setTitle(e.target.value)} required></input>
+                <textarea type="text" placeholder='Text' id='input-text' rows='8' onChange={(e) => setContent(e.target.value)} required></textarea>
+                <button type='submit'>Post</button>
+            </form>
+
         </div>
     );
 }
