5edfe88761692def27f1b15ec057a371df9c1aa9
Kaedan <kd577631@dal.ca>
2024-06-28 17:04:03 -0300

    feat: added useEffect to grab posts

--- frontend/dal-social/src/components/feed.jsx
+++ frontend/dal-social/src/components/feed.jsx
@@ -13,9 +13,43 @@ const Feed = () => {
     const[user_Id, setUserId] = useState('');
 
     const[isExpanded, setIsExpanded] = useState(false);
+    const[email, setEmail] = useState('');
 
     const navigate = useNavigate();
 
+
+    const[posts, setPosts] = useState([]);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try{
+                const response = await axios.get('http://localhost:8080/posts/fetch/3');
+                if(!response){
+                    throw new Error("Failed to grab posts");
+                }
+                const data = await response.json();
+                setPosts(data);
+            }
+            catch(error){
+                setError(error);
+            }
+            finally{
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    if(loading){
+        return <div>Loading...</div>;
+    }
+
+    if(error){
+        return <div>Error: {error.message}</div>;
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -57,6 +91,7 @@ const Feed = () => {
             <h2>DALHOUSIE SOCIAL NETWORK</h2>
 
             <nav className='nav'>
+                <button id='nav-button' onClick={() => navigate('/profile/' + localStorage.getItem('loggedInUser'))}>Profile</button>
                 <button aria-expanded={isExpanded} id='nav-button' onClick={toggleVisible}>Create Post</button>
                 <Logout className='logout'/>
             </nav>
@@ -70,6 +105,18 @@ const Feed = () => {
                     <textarea type="text" placeholder='Text' id='input-text' rows='8' onChange={(e) => setContent(e.target.value)} required></textarea>
                     <button type='submit' id='post-button'>Post</button>
                 </form>
+            </div>
+
+            <div className='grid-containter'>
+                <div className='grid-item'>1</div>
+                <div className='grid-item'>2</div>
+                <div className='grid-item'>3</div>
+                <div className='grid-item'>4</div>
+                <div className='grid-item'>5</div>
+                <div className='grid-item'>6</div>
+                <div className='grid-item'>7</div>
+
+
             </div>
 
         </div>
