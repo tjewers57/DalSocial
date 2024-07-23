684fd7de928f269cd81fa7cdfeeabfdbd60d81b9
Aryan Patel <aryan.patel@dal.ca>
2024-07-23 19:39:28 -0300

    feat: "updated post.jsx file with likes and dislikes UI on posts"

--- frontend/dal-social/src/components/post.jsx
+++ frontend/dal-social/src/components/post.jsx
@@ -1,43 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import '../css/post.css';
-import { Link, useNavigate} from 'react-router-dom';
 
-const Post = (post) => {
-    const[firstName, setFirstName] = useState('');
-    const[lastName, setLastName] = useState('');
-    const[email, setEmail] = useState('');
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        getUser();
-    }, []);
-
-    const getUser = async () => {
-        try {
-            const user = await axios.get('http://localhost:8080/users/get/' + post.post.userId);
-            setFirstName(user.data.firstName);
-            setLastName(user.data.lastName);
-            setEmail(user.data.email);
-        } catch (error) {
-            console.log(error)
-            alert("An error occured, please try again.");
-        }
-
-    }
-
+const Post = ({ post, onLike, onDislike }) => {
     return (
-        <div className='postTemplate'>
-            <h2 className='post-title'>{post.post.title}</h2>
-            <pre className='post-content'>
-                {post.post.content}
-            </pre>
-            <p className='post-date'>
-                Posted: <strong>{post.post.postDate}</strong> by <a className='post-profile' onClick={() => navigate('/profile/' + email)}>{firstName} {lastName}</a>
-            </p>
+        <div className='post'>
+            <h4>{post.title}</h4>
+            <p>{post.content}</p>
+            <div className='postActions'>
+                <button onClick={onLike}>Like</button>
+                <span>{post.likes}</span>
+                <button onClick={onDislike}>Dislike</button>
+                <span>{post.dislikes}</span>
+            </div>
         </div>
     );
-}
+};
 
 export default Post;
