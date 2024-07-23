ae170c894d11d9dd6a8dbd2669706c597090099b
Aryan Patel <aryan.patel@dal.ca>
2024-07-23 19:27:52 -0300

    feat: "updated post service class  for like and dislike fields"

--- backend/dalsocial/src/main/java/com/csci3130/group7/dalsocial/service/PostService.java
+++ backend/dalsocial/src/main/java/com/csci3130/group7/dalsocial/service/PostService.java
@@ -5,15 +5,11 @@ import com.csci3130.group7.dalsocial.model.Post;
 import java.util.List;
 
 public interface PostService {
-
     String createPost(Post post);
-
     List<Post> fetchAllPostsByUserId(Integer userId);
-
     Post findPostById(Integer id);
-
     String deletePost(Integer id);
-
     String updatePost(Post post);
+    String likePost(Integer id);
+    String dislikePost(Integer id);
 }
-
