288938d7f7bb78b41984531eca86ffc36818bf19
Aryan Chaudhari <ar321922@dal.ca>
2024-07-25 17:17:23 -0300

    Fix: bug fixed for the redirecting the page based on role

--- frontend/dal-social/src/components/admin.jsx
+++ frontend/dal-social/src/components/admin.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import '../css/friends.css';
+import '../css/admin.css';
 
 function AdminListComponent() {
     const { friend } = useParams();
