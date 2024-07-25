69154460a51eba8c0eb50d288464bf478f5fdb9d
Kaedan <kd577631@dal.ca>
2024-07-24 23:45:12 -0300

    frontend done, need to make backend functionality

---
+++ frontend/dal-social/src/components/blocked.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+const Blocked = (userEmail) => {
+    const[isBlocked, setIsBlocked] = useState(false);
+
+    useEffect(() => {
+        getStatus();
+    }, [])
+
+    const getStatus = async () => {
+        try {
+            const currentUser = await axios.get('http://localhost:8080/users/getbyemail/' + localStorage.getItem('loggedInUser'));
+            const targetUser = await axios.get('http://localhost:8080/users/getbyemail/' + userEmail.userEmail);
+            if(currentUser.data == '' || targetUser.data == ''){
+                alert("Error, invalid user.");
+            }
+            
+            //this will grab the status of the relationship between the two users
+            const blockStatus = await axios.get('http://localhost:8080/friend-requests/checkrequeststatus/' + currentUser.data.id + '/' + targetUser.data.id);
+            if(blockStatus.data){
+                setIsBlocked(true);
+            }
+        } catch (error) {
+            console.log(error);
+            alert("An error occured, please try again.");
+        }
+    }
+
+    const blockUser = async () => {
+        try {
+            const currentUser = await axios.get('http://localhost:8080/users/getbyemail/' + localStorage.getItem('loggedInUser'));
+            const targetUser = await axios.get('http://localhost:8080/users/getbyemail/' + userEmail.userEmail);
+            if(currentUser.data == '' || targetUser.data == ''){
+                alert("Error, invalid user.");
+            }
+
+            //this will modify the binary value of blocked for the target user
+            //const requestStatus = await axios.get();
+        }
+        catch (error) {
+            console.log(error);
+            alert("An error occured, please try again.");
+        }
+    }
+
+    const unblockUser = async () => {
+        try {
+            const currentUser = await axios.get('http://localhost:8080/users/getbyemail/' + localStorage.getItem('loggedInUser'));
+            const targetUser = await axios.get('http://localhost:8080/users/getbyemail/' + userEmail.userEmail);
+            if(currentUser.data == '' || targetUser.data == ''){
+                alert("Error, invalid user.");
+            }
+
+            //this will modify the binary value of blocked for the target user
+            //const requestStatus = await axios.get();
+        }
+        catch (error) {
+            console.log(error);
+            alert("An error occured, please try again.");
+        }
+    }
+
+    return(
+        <div>
+            {isBlocked && (
+                <div>
+                    <button onclick={unblockUser}>Unblock</button>
+                    <h2>You cannot see this user's posts as you are blocked.</h2>
+                    <p>Better luck next time, chum.</p>
+                </div>
+            )}
+
+            {!isBlocked &&
+                <button onclick={blockUser}>BLOCK</button>
+            }
+
+        </div>
+    );
+}
